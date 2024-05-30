e60b6e1bf906e28d04bb3eca66a6577f0edb754b
wippy <lucchini.william@gmail.com>
2024-05-30 12:45:49 +0000

    versione multiprocesso

---
+++ multiProcess/bigrammi.c
@@ -0,0 +1,290 @@
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <float.h>
+
+#include <wchar.h>
+#include <wctype.h>
+
+#include "../lib/bilist.h"
+#include "../lib/sublist.h"
+#include "../lib/utils.h"
+
+/* # Test only */
+#include <time.h>
+
+/*
+Error 01: Errore apertura file
+Error 02:
+Error 03:
+*/
+
+/**
+ * Rimuove gli zeri inutili da una stringa.
+ *
+ * @param str La stringa da modificare.
+ */
+void removeZero(char *str)
+{
+    if (str[0] == '1' && str[1] == '.' && str[2] == '0')
+    {
+        strcpy(str, "1");
+        return;
+    }
+    for (int i = 7; i >= 3; i--)
+    {
+        char lettera = str[i];
+        int ascii_char = (int)lettera;
+
+        if (ascii_char == 48 || ascii_char == 0)
+        {
+            str[i] = '\0';
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return;
+}
+
+/*      Funzioni Programma       */
+
+/**
+ * Genera una lista di parole a partire da un file.
+ *
+ * @param file Il file da cui leggere le parole.
+ * @return Un puntatore alla testa della lista di parole generate.
+ */
+nodo_t *genera_lista(FILE *file)
+{
+    int indice_parola = 0;
+    wchar_t character;
+    wchar_t parola[31];
+
+    pulisci(parola, 31);
+
+    nodo_t *lista = NULL; // DEVI INIZIALIZZARE A NULL
+
+    insert(&lista, L".");
+
+    wchar_t prima_parola[31];
+    pulisci(prima_parola, 31);
+
+    while ((character = fgetwc(file)) != WEOF)
+    {
+        character = towlower(character);
+        // se il carattere è uno spazio o si va a capo, viene aggiunta la parola alla lista
+        if ((character == ' ' || character == '\n') && indice_parola > 0)
+        {
+            parola[indice_parola] = '\0';
+
+            insert(&lista, parola);
+            if (prima_parola[0] == '\0')
+            {
+                wcscpy(prima_parola, parola);
+            }
+
+            pulisci(&parola, indice_parola);
+
+            indice_parola = 0;
+        }
+        // aggiunge come parola i caratteri di punteggiatura
+        else if (character == '.' || character == '?' || character == '!')
+        {
+            parola[indice_parola] = '\0';
+
+            insert(&lista, parola);
+            if (prima_parola[0] == '\0')
+            {
+                wcscpy(prima_parola, parola);
+            }
+
+            parola[0] = character;
+            parola[1] = '\0';
+
+            insert(&lista, parola);
+            if (prima_parola[0] == '\0')
+            {
+                wcscpy(prima_parola, parola);
+            }
+
+            pulisci(&parola, indice_parola);
+
+            indice_parola = 0;
+        }
+        // aggiunge alla parola l'apostrofo e termina la parola
+        else if (character == '\'')
+        {
+            parola[indice_parola] = character;
+            indice_parola++;
+            parola[indice_parola] = '\0';
+            insert(&lista, parola);
+            if (prima_parola[0] == '\0')
+            {
+                wcscpy(prima_parola, parola);
+            }
+            pulisci(&parola, indice_parola);
+            indice_parola = 0;
+        }
+        // altrimenti aggiunge la lettera alla parola
+        else if (iswalnum(character))
+        {
+            // se c'è una parola maggiore di 30 caratteri troncala
+            if (indice_parola > 30)
+            {
+                indice_parola = 0;
+                printf("%ls \n", parola);
+            }
+
+            parola[indice_parola] = character;
+            indice_parola++;
+        }
+    }
+    // se rimane una parola nel buffer la aggiunge
+    if (indice_parola > 0)
+    {
+        parola[indice_parola] = '\0';
+        indice_parola = 0;
+        insert(&lista, parola);
+    }
+
+    insert(&lista, prima_parola);
+
+    return lista;
+}
+
+Node_t *genera_bigrammi(nodo_t *lista_parole)
+{
+    Node_t *lista_bigrammi = NULL;
+
+    wchar_t buffer[31];
+    pulisci(buffer, 31);
+
+    while (lista_parole != NULL)
+    {
+        if (buffer[0] == '\0')
+        {
+            wcscpy(buffer, lista_parole->val);
+            lista_parole = lista_parole->next_p;
+            continue;
+        }
+        magia(&lista_bigrammi, lista_parole->val, buffer);
+        wcscpy(buffer, lista_parole->val);
+        lista_parole = lista_parole->next_p;
+    }
+    return lista_bigrammi;
+    // printf("%ls %ls \n", lista_parole->val, buffer);
+}
+
+void genera_file(FILE *file, Node_t *head)
+{
+    SubNode_t *sub_head = NULL;
+    float freq = 0;
+    int len = 0;
+
+    while (head != NULL)
+    {
+        fprintf(file, "%ls", head->val);
+
+        // Stampiamo la sottolista, se presente
+        if (head->inner_p == NULL)
+        {
+            continue;
+        }
+        SubNode_t *sub_head = head->inner_p;
+        // fprintf(file, "  (");
+        fprintf(file, ",");
+        len = sb_innerlen(sub_head);
+        while (sub_head != NULL)
+        {
+            if (len > 1)
+            {
+                freq = (float)sub_head->freq / len;
+            }
+            else
+            {
+                freq = 1;
+            }
+
+            char str[8];
+            sprintf(str, "%.6f", freq);
+            removeZero(str);
+            fprintf(file, "%ls,%s", sub_head->val, str);
+
+            if (sub_head->next_p != NULL)
+            {
+                fprintf(file, ",");
+            }
+
+            sub_head = sub_head->next_p;
+        }
+        // fprintf(file, ")\n");
+        fprintf(file, "\n");
+
+        head = head->next_p;
+    }
+}
+
+/*      Programma       */
+
+void main_bigrammi(char *input_path, char *output_path)
+{
+    setlocale(LC_ALL, "");
+    setlocale(LC_NUMERIC, "en_US.UTF-8");
+
+    int pid1;
+    int pid2;
+
+    clock_t begin;
+    clock_t end;
+
+    nodo_t *lista_parole = NULL;
+    Node_t *lista_bigrammi = NULL;
+
+    FILE *input_file = open_file(input_path, "r");
+
+    begin = clock();
+    lista_parole = genera_lista(input_file);
+    end = clock();
+    printf("Genera Lista: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\nlen lista: %d parole\n", len(lista_parole));
+
+    pid1 = fork();
+
+    if (pid1 == 0)
+    {
+        begin = clock();
+        lista_bigrammi = genera_bigrammi(lista_parole);
+        end = clock();
+        printf("Genera bigrammi: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+        
+        pid2 = fork();
+        if (pid2 == 0)
+        {
+            FILE *output_file = open_file(output_path, "w+");
+            begin = clock();
+            genera_file(output_file, lista_bigrammi);
+            end = clock();
+            printf("Genera file: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+            exit(0);
+        }
+        else
+        {
+            wait(NULL);
+        }
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+    }
+
+}
+// int main()
+// {
+//     main_bigrammi("../test/lotr.txt", "bigrammi.csv");
+//     return 0;
+// }
---
+++ multiProcess/generatore.c
@@ -0,0 +1,407 @@
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <wchar.h>
+#include <wctype.h>
+
+#include <math.h>
+
+#include "../lib/bilist.h"
+#include "../lib/sublist.h"
+#include "../lib/utils.h"
+
+/* # Test only */
+#include <time.h>
+
+/*
+Error 01: Errore apertura file
+Error 02:
+Error 03:
+*/
+
+void printSubList_csv(Node_t *head)
+{
+    printf("\n\n\n");
+
+    while (head != NULL)
+    {
+        printf("%ls", head->val);
+
+        // Stampiamo la sottolista, se presente
+        if (head->inner_p != NULL)
+        {
+            SubNode_t *sub_head = head->inner_p;
+            printf(" (");
+            while (sub_head != NULL)
+            {
+                printf("%ls %.4f", sub_head->val, sub_head->freq);
+                if (sub_head->next_p != NULL)
+                {
+                    printf(" -> ");
+                }
+
+                sub_head = sub_head->next_p;
+            }
+
+            printf(")");
+        }
+
+        if (head->next_p != NULL)
+        {
+            printf("\n");
+        }
+
+        head = head->next_p;
+    }
+    printf("\n\n");
+}
+
+/*      Funzioni Programma       */
+
+/**
+ * Legge un file e estrae i bigrammi da esso.
+ *
+ * @param file Il file da leggere.
+ * @return Un puntatore alla testa della lista concatenata contenente i bigrammi estratti.
+ */
+Node_t *read_file(FILE *file)
+{
+    Node_t *lista_bigrammi = NULL;
+
+    int indice_parola = 0; /* Traccia della lunghezza della parola  */
+    wchar_t character;     /* carattere corrente */
+    wchar_t parola[31];    /* buffer per la parola */
+    pulisci(parola, 31);
+
+    wchar_t bigramma_parola[31]; /* buffer per la parola del bigramma */
+    pulisci(bigramma_parola, 31);
+    float bigramma_numero = 0; /* buffer per il numero del bigramma */
+
+    float sub_counter = 0;
+
+    Node_t *head_pointer = NULL; /* Traccia la testa del nodo da dove inizia sottolista */
+
+    while ((character = fgetwc(file)) != WEOF)
+    {
+        character = towlower(character);
+
+        /* se il carattere è una virgola, viene processata la parola */
+        if ((character == ',') && indice_parola > 0)
+        {
+            parola[indice_parola] = '\0';
+
+            if (head_pointer == NULL)
+            {
+                head_pointer = sb_append(&lista_bigrammi, parola);
+
+                sub_counter = 0;
+
+                pulisci(&parola, indice_parola);
+                indice_parola = 0;
+                continue;
+            }
+            if (bigramma_parola[0] == '\0')
+            {
+
+                wcscpy(bigramma_parola, parola);
+                pulisci(&parola, indice_parola);
+                indice_parola = 0;
+                continue;
+            }
+
+            if (swscanf(parola, L"%f", &bigramma_numero) == 1)
+            {
+                // Conversione riuscita, stampa il valore float
+                // printf("Valore float: %f\n", bigramma_numero);
+            }
+            else
+            {
+                // Conversione fallita
+                // printf("Errore durante la conversione\n");
+            }
+            sb_insertToInner(head_pointer, bigramma_parola, bigramma_numero);
+
+            sub_counter += bigramma_numero;
+
+            pulisci(&parola, indice_parola);
+            indice_parola = 0;
+            bigramma_parola[0] = '\0';
+        }
+        /* se il carattere è un a capo, viene inserita la coppia parola-valore e vengono resettate le variabili */
+        else if (character == '\n')
+        {
+            if (swscanf(parola, L"%f", &bigramma_numero) == 1)
+            {
+                // Conversione riuscita, stampa il valore float
+                // printf("Valore float: %f\n", bigramma_numero);
+            }
+            else
+            {
+                // Conversione fallita
+                // printf("Errore durante la conversione\n");
+            }
+            sb_insertToInner(head_pointer, bigramma_parola, bigramma_numero);
+
+            sub_counter += bigramma_numero;
+
+            if (sub_counter > 1.000334 || sub_counter < 0.999673 )
+            {
+                printf("\nErrore: File csv malformato; La somma delle frequenze è diversa da 1 : %f\n", sub_counter);
+                // return lista_bigrammi;
+            }
+
+            head_pointer = NULL;
+            bigramma_parola[0] = '\0';
+            pulisci(&parola, indice_parola);
+            indice_parola = 0;
+
+            // printSubList_csv(lista_bigrammi);
+        }
+        /* per qualsiasi altro caso viene aggiunto il carattere al buffer */
+        else
+        {
+            /* se c'è una parola maggiore di 30 caratteri la tronca */
+            if (indice_parola > 30)
+            {
+                indice_parola = 0;
+                printf("%ls \n", parola);
+            }
+
+            parola[indice_parola] = character;
+            indice_parola++;
+        }
+    }
+    // se rimane una parola nel buffer la aggiunge
+    // if (indice_parola > 0)
+    // {
+    //     parola[indice_parola] = '\0';
+    //     indice_parola = 0;
+    //     process(&lista, parola);
+    // }
+
+    return lista_bigrammi;
+}
+
+/**
+ * Genera una lista di parole in base ai bigrammi presenti nella lista dei bigrammi fornita.
+ *
+ * @param lista_bigrammi La lista dei bigrammi.
+ * @param n_parole Il numero di parole da generare.
+ * @param parola_iniziale La parola da cui iniziare la generazione delle parole. Se è una stringa vuota, verrà scelta una parola iniziale casuale tra '.', '?', '!'.
+ * @return La lista di parole generate. Se si verifica un errore, viene restituito un valore negativo.
+ */
+nodo_t *genera_parole(Node_t *lista_bigrammi, int n_parole, wchar_t *parola_iniziale)
+{
+    srand(time(0));
+    // srand(0);
+
+    int random;
+    Node_t *head = NULL;
+    nodo_t *lista_parole = NULL;
+
+    if (wcslen(parola_iniziale) <= 0)
+    {
+        wchar_t caratteri_iniziali[4] = {L'.', L'?', L'!', '\0'};
+        wchar_t scelta[2] = {'\0', '\0'};
+        int tentativi[3] = {0, 0, 0};
+
+        do
+        {
+            random = ((double)rand() / RAND_MAX) * (3 - 0) + 0;
+            scelta[0] = caratteri_iniziali[random];
+            head = sb_find(lista_bigrammi, scelta);
+            tentativi[random] = 1;
+
+            // printf("\nscelta: %ls\n", scelta);
+            // printf("stato array: %d %d %d \n", tentativi[0],tentativi[1],tentativi[2]);
+
+        } while (head == NULL && (tentativi[0] == 0 || tentativi[1] == 0 || tentativi[2] == 0));
+
+        if (head == NULL)
+        {
+            printf("\nErrore: Il testo non contiene nessuno dei caratteri di default ( . ? ! ), fornire una parola da cui iniziare\n");
+            exit(-1);
+            return NULL;
+        }
+    }
+    else
+    {
+        
+        toLowerCase(parola_iniziale);
+
+        head = sb_find(lista_bigrammi, parola_iniziale);
+        if (head == NULL)
+        {
+            printf("\nErrore: La parola/carattere '%ls' non è presente nel testo\n", parola_iniziale);
+            exit(-1);
+            return -1;
+        }
+    }
+
+    SubNode_t *sub_head = NULL;
+    float valore = 0;
+
+    for (int i = 0; i < n_parole; i++)
+    {
+        random = ((double)rand() / RAND_MAX) * (100000 - 0) + 0;
+        sub_head = head->inner_p;
+        while (sub_head != NULL)
+        {
+            valore += (sub_head->freq * 100000);
+            if (valore >= random)
+            {
+                valore = 0;
+                break;
+            }
+
+            sub_head = sub_head->next_p;
+            if (sub_head == NULL)
+            {
+                printf("\n Round error");
+                return;
+            }
+        }
+        insert(&lista_parole, sub_head->val);
+
+        head = sb_find(lista_bigrammi, sub_head->val);
+
+        if (head == NULL)
+        {
+            head = lista_bigrammi;
+            // printf("\nErrore: Lista malformata; Data la parola: %ls non è possibile generarne un'altra\n", parola_iniziale);
+            // exit(-1);
+            // return lista_parole;
+        }
+    }
+
+    return lista_parole;
+
+    // printf("numero random: %d\n", caratteri_iniziali[random]);
+    // printf("Stringa: %ls\n", caratteri_iniziali);
+}
+
+/**
+ * Funzione per generare un file di testo a partire da una lista concatenata di nodi.
+ *
+ * La funzione prende in input un puntatore a un file e un puntatore al nodo di testa della lista.
+ * Scorre la lista e stampa le parole nel file, con alcune regole specifiche:
+ * - La prima lettera della prima parola viene convertita in maiuscolo e stampata nel file.
+ * - Se una parola è seguita da un carattere di punteggiatura (. ? !), la parola viene stampata nel file,
+ *   seguita dal carattere di punteggiatura e una nuova riga.
+ *   Successivamente, la parola successiva viene convertita in maiuscolo e stampata nel file.
+ * - Altrimenti, la parola viene semplicemente stampata nel file.
+ *
+ * @param file Il puntatore al file in cui scrivere il testo generato.
+ * @param head Il puntatore al nodo di testa della lista concatenata.
+ */
+void genera_file_generatore(FILE *file, nodo_t *head)
+{
+    short unsigned maiusc_flag = 0;
+    nodo_t *next = NULL;
+
+    nodo_t *tail = NULL;
+    head = getTail(head);
+
+    head->val[0] = towupper(head->val[0]);
+    fprintf(file, "%ls", head->val);
+    head = head->prev_p;
+
+    while (head != NULL)
+    {
+        if (head->prev_p != NULL)
+            next = head->prev_p;
+
+        if (next->val[0] == L'.' || next->val[0] == L'?' || next->val[0] == L'!')
+        {
+            /* stampa la parola prima del carattere di punteggiatura */
+            fprintf(file, " %ls", head->val);
+
+            /* avanza al carattere di punteggiatura e lo stampa */
+            if (head->prev_p != NULL)
+                head = head->prev_p;
+            else
+                break;
+            // fprintf (file, "%ls", head->val);
+            fprintf(file, "%ls\n", head->val);
+
+            /* avanza alla parola dopo, mette la maiuscola e stampa la parola */
+            if (head->prev_p != NULL)
+                head = head->prev_p;
+            else
+                break;
+            head->val[0] = towupper(head->val[0]);
+            // fprintf(file, " %ls", head->val);
+            fprintf(file, "%ls", head->val);
+            head = head->prev_p;
+            continue;
+        }
+
+        fprintf(file, " %ls", head->val);
+        head = head->prev_p;
+    }
+}
+
+/*      Main       */
+void main_generatore(char *input_path, char *n_words, wchar_t *search_word, char *output_path)
+{
+    setlocale(LC_ALL, "");
+    setlocale(LC_NUMERIC, "en_US.UTF-8");
+
+    int pid1 = -1;
+    int pid2 = -1;
+
+    nodo_t *lista_parole = NULL;
+    Node_t *lista_bigrammi = NULL;
+
+    clock_t begin;
+    clock_t end;
+
+    FILE *input_file = open_file(input_path, "r");
+
+    begin = clock();
+    lista_bigrammi = read_file(input_file);
+    end = clock();
+    printf("\nAnalizzare csv: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+    
+    pid1 = fork();
+
+    // printf("pid1: %d\n ",pid1);
+
+    if (pid1 == 0)
+    {
+        int n_words_int = atoi(n_words);
+
+        begin = clock();
+        lista_parole = genera_parole(lista_bigrammi, n_words_int, search_word);
+        end = clock();
+        printf("Generatore parole: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+        pid2 = fork();
+        if (pid2 == 0)
+        {
+            FILE *output_file = open_file(output_path, "w");
+
+            begin = clock();
+            genera_file_generatore(output_file, lista_parole);
+            end = clock();
+            printf("Generatore file: %fs\n", (double)(end - begin) / CLOCKS_PER_SEC);
+            exit(0);
+        }
+        else
+        {
+            wait(NULL);
+        }
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+    }
+}
+ 
+// int main()
+// {
+//     main_generatore("out.csv", "100", L"Hitaghi","out.txt");
+//     return 0;
+// }
